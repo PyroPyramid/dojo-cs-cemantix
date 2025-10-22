d08a7a0f4262767fdc5354089e49bb28e5737711
Noureldin Adham <adham.noureldin@student-cs.fr>
2025-10-22 14:17:02 +0200

    Changes done

--- dojo-cs-cemantics/src/app/service/api-client.service.ts
+++ dojo-cs-cemantics/src/app/service/api-client.service.ts
@@ -11,6 +11,6 @@ export class ApiClientService {
   private httpClient = inject(HttpClient);
 
   public getPokemonValue(guess: GuessWord): Observable<ResponseWord>{
-    return this.httpClient.get<ResponseWord>(`https://pokeapi.co/api/v2/pokemon/${guess}`)
+    return this.httpClient.get<ResponseWord>(`https://deno-hello-world-ze5xm8bbkzbx.pyropyramid.deno.net/${guess}`)
   }
 }
